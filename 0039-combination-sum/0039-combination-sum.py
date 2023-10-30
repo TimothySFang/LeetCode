e3fad0c6100b50248d751c0f67d74f29cd330312
Timothy Fang <56328137+Timothy-S-Fang@users.noreply.github.com>
2023-10-31 00:06:30 +0100

    Time: 48 ms (28.48%), Space: 13.2 MB (90.85%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        result = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                result.append(list(cur))
+                return
+            if total > target or i > len(candidates) - 1:
+                return
+            
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+
+        dfs(0, [], 0)
+        return result
+
