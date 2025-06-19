08563856f2471d6c5a59be6db7e6a3407dd58ca2
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-06-19 12:46:14 -0700

    Time: 19 ms (32.97%), Space: 35 MB (10.04%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,9 +4,7 @@ class Solution:
         for n in nums:
             if n in visited:
                 visited[n] += 1
+                return True
             else:
                 visited[n] = 1
-        for n in visited:
-            if visited[n] != 1:
-                return True
         return False
