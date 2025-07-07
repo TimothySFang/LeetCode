f71e6ffdab7f26f0a61a78e9d8f9b9e63a8d2d7d
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-07-07 16:25:49 -0700

    Time: 583 ms (56.09%), Space: 20.7 MB (68.95%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,20 +1,20 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         result = []
-        for i in range(len(nums)):
-            target = -1 * nums[i]
-            target_index = i
-            visited = {}
-            for j in range(i, len(nums)):
-                if (j != target_index):
-                    if (target - nums[j]) in visited:
-                        potAns = [-1 * target, nums[j], (target - nums[j])]
-                        potAns.sort()
-                        if potAns not in result: 
-                            result.append(potAns)
-                    else: visited[nums[j]] = nums[j]
+        for i, n  in enumerate(nums):
+            if i > 0 and n == nums[i - 1]:
+                continue  
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                current = n + nums[l] + nums[r]
+                if current == 0:
+                    result.append([n, nums[l], nums[r]])
+                    l += 1
+                    while(nums[l] == nums[l - 1] and l < r):
+                        l += 1
+                elif current < 0:
+                    l += 1
+                elif current > 0:
+                    r -= 1
         return result
