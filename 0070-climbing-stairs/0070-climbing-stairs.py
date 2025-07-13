25a061d570cac0b6c3a7015b3abfb03a738d42f8
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-07-13 16:33:56 -0700

    Time: 0 ms (100%), Space: 17.7 MB (48.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,22 +1,14 @@
-class Solution(object):
-    from collections import defaultdict
-
-    def countStairsMemo(self, n, memo):
-        if memo[n] > 0 :
-            return memo[n]
-
-        memo[n] = self.countStairsMemo(n - 1, memo) + self.countStairsMemo(n - 2, memo)
-        return memo[n]
-
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        
-        memo = defaultdict(lambda: -1)
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def memoizeStairs(memo, n):
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n] = memoizeStairs(memo, n - 1) + memoizeStairs(memo, n - 2)
+                return memo[n]
+        memo = {}
         memo[1] = 1
         memo[2] = 2
         memo[3] = 3
-        return self.countStairsMemo(n, memo)
-
+        memoizeStairs(memo, n)
+        return memo[n]
