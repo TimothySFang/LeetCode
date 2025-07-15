abdc1127626e478c73a4fe8168216cb3822afb26
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-07-14 18:00:29 -0700

    Time: 0 ms (100%), Space: 18.1 MB (71.76%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,30 +1,29 @@
 class Solution:
-    def binMatSearch(self, matrix: List[List[int]], target: int, left: int, right: int) -> int:
-        if right < left or matrix[left][0] > target or matrix[right][-1] < target:
-            return -1
-        middle = left + ((right - left) // 2)
-        if (matrix[middle][0] <= target <= matrix[middle][-1]):
-            return middle
-        elif (matrix[middle][0] > target):
-            # search left
-            return self.binMatSearch(matrix, target, left, middle - 1)
-        else: 
-            return self.binMatSearch(matrix, target, middle + 1, right)
-    
-    def binSearch(self, array: List[int], target: int, left: int, right: int) -> bool:
-        if right < left:
-            return False
-        middle = left + ((right - left) // 2)
-
-        if (array[middle] == target):
-            return True
-        elif (array[middle] < target):
-            return self.binSearch(array, target, middle + 1, right)
-        else:
-            return self.binSearch(array, target, left, middle - 1)
-
-
-
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        matrix_ind = self.binMatSearch(matrix, target, 0, len(matrix) - 1)
-        return self.binSearch(matrix[matrix_ind], target, 0, len(matrix[matrix_ind]) - 1)
+        def binarySearch(nums, left, right):
+            middle = (left + right) // 2
+            if left > right:
+                return False
+            if nums[middle] == target:
+                return True
+            elif target < nums[middle]:
+                # search left
+                return binarySearch(nums, left, middle - 1)
+            elif target > nums[middle]:
+                return binarySearch(nums, middle + 1, right)
+
+        def binSearchRow(mat):
+            if not mat:
+                return False
+            middle = (len(mat) - 1) // 2
+            if target >= mat[middle][0] and target <= mat[middle][-1]:
+                #found it
+                return binarySearch(mat[middle], 0, len(mat[middle]) - 1)
+            elif target < mat[middle][0]:
+                # search left
+                return binSearchRow(mat[: middle])
+            elif target > mat[middle][-1]:
+                # search right
+                return binSearchRow(mat[middle + 1 :])
+            
+        return binSearchRow(matrix)
