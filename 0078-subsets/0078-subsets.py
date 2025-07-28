20199e5de0eccbc7859b16902311b9b390de81f1
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-07-28 15:37:46 -0700

    Time: 0 ms (100%), Space: 17.7 MB (90.95%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,21 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        soln = [[]]
-        def backtrackSubsets(nums):
-            nonlocal soln
-            if len(nums) == 0:
-                # basecase (end state)
-                return soln
+        soln = []
+        def backtrackSubsets(start, path):
+            soln.append(path[:])
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrackSubsets(i + 1, path)
+                path.pop()
+        backtrackSubsets(0, [])
             
-            curr = nums.pop()
-            soln_copy = [s[:] for s in soln]
-            for n in range(len(soln_copy)):
-                soln_copy[n].append(curr)
-            soln = soln + soln_copy
-            # ^ added all possible permutes for curr
-            return backtrackSubsets(nums)
-
-        return backtrackSubsets(nums)
-
-
-            
+        return soln
