70753e32430873bd51b645933821c4a26d86ac32
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-07-28 14:56:43 -0700

    Time: 0 ms (100%), Space: 17.8 MB (65.22%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,21 @@
-class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        ans = [[]]
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        soln = [[]]
+        def backtrackSubsets(nums):
+            nonlocal soln
+            if len(nums) == 0:
+                # basecase (end state)
+                return soln
+            
+            curr = nums.pop()
+            soln_copy = [s[:] for s in soln]
+            for n in range(len(soln_copy)):
+                soln_copy[n].append(curr)
+            soln = soln + soln_copy
+            # ^ added all possible permutes for curr
+            return backtrackSubsets(nums)
 
-        for i in range(len(nums)):
-            temp_ans = []
-            for j in range(len(ans)):
-                temp_ans.append(ans[j])
-                temp_ans.append(ans[j] + [nums[i]])
-            ans = temp_ans
+        return backtrackSubsets(nums)
 
-        return ans
+
+            
