8965a84ce4e59bdb24901199e38a77c9cf937688
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-07-28 22:37:04 -0700

    Time: 0 ms (100%), Space: 17.8 MB (34.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,16 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        memo_array = {}
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
 
-        for i in range(len(nums)):
-            if i == 0:
-                memo_array[0] = nums[i]
-            elif i == 1:
-                memo_array[1] = max(nums[i], memo_array[0])
-            else:
-                memo_array[i] = max(nums[i] + memo_array[i - 2], memo_array[i - 1])
-        
-        return memo_array[len(nums) - 1]
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        return dp[-1]
