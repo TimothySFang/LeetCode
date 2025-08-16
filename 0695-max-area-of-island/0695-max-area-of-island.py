47b5c2fd3b249b1f419c4c9b0bf52fd674285e4f
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-08-15 19:40:31 -0700

    Time: 30 ms (32.45%), Space: 19.5 MB (16.76%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,28 +2,21 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         visited = set()
         max_size = 0
-        H, W = len(grid), len(grid[0])
 
-        def bfs(y, x):
-            # out of bounds or water or already seen
-            if not (0 <= y < H and 0 <= x < W): 
+        def dfs(y, x):
+            if not (0 <= y < len(grid) and 0 <= x < len(grid[0])):
                 return 0
             if grid[y][x] != 1 or (y, x) in visited:
                 return 0
-
+                
             visited.add((y, x))
-            # count this cell + neighbors
-            return (
-                1
-                + bfs(y + 1, x)
-                + bfs(y - 1, x)
-                + bfs(y, x + 1)
-                + bfs(y, x - 1)
-            )
+            
+            return (1 + dfs(y + 1, x) + dfs(y - 1, x) + dfs(y, x + 1) + dfs(y, x - 1))
 
-        for y in range(H):
-            for x in range(W):
+
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
                 if (y, x) not in visited and grid[y][x] == 1:
-                    max_size = max(max_size, bfs(y, x))
-
+                    max_size = max(max_size, dfs(y, x))
         return max_size
+
