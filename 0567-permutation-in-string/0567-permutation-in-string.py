e592623e200be28b1028654283a7a936bb3560d1
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-08-25 13:54:01 -0700

    Time: 15 ms (84.71%), Space: 17.8 MB (63.17%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,22 +1,24 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-        characters = {}
-        for c in s1:
-            if c in characters:
-                characters[c] += 1
-            else: characters[c] = 1
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        curr_string = [0] * 26
+        s1_array = [0] * 26
+
+        # populate the left most curr window and s1_array
+        for i in range(len(s1)):
+            s1_array[ord(s1[i]) - ord('a')] += 1
+            curr_string[ord(s2[i]) - ord('a')] += 1
         
-        for i in range(len(s2) - len(s1) + 1):
-            windowChars = {}
-            for j in range(i, i + len(s1)):
-                if s2[j] in windowChars:
-                    windowChars[s2[j]] += 1
-                else: windowChars[s2[j]] = 1
-            if characters == windowChars:
+        l, r = 0, len(s1)
+        if curr_string == s1_array:
+            return True
+        while r < len(s2):
+            curr_string[ord(s2[l]) - ord('a')] -= 1
+            curr_string[ord(s2[r]) - ord('a')] += 1
+            l += 1
+            r += 1
+
+            if curr_string == s1_array:
                 return True
         return False
