59e4e1b451d4f5ca894dd5a18f6787c707c6f041
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-09-11 13:07:44 -0700

    Time: 0 ms (100%), Space: 18.6 MB (72.12%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -3,14 +3,9 @@ from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        xor_all = 0
-
-        # XOR all expected values: 0..n
-        for i in range(n + 1):
-            xor_all ^= i
-
-        # XOR with all values in the list
-        for num in nums:
-            xor_all ^= num
-
-        return xor_all
+        res = 0
+        for i in range(1, n + 1):
+            res ^= i
+        for n in nums:
+            res ^= n
+        return res
