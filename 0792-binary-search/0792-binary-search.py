145b543bf5b004af483b669b834f83f9894bc40d
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-11-14 19:57:57 -0800

    Time: 0 ms (100%), Space: 18.6 MB (73.29%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,15 +1,19 @@
 class Solution:
-    def binSearch(self, nums: List[int], left: int, right: int, target: int) -> int:
-        if left > right:
-            return -1
-        middle = ((left + right) // 2)
-        if target == nums[middle]:
-            return middle
-        elif nums[middle] < target:
-            return self.binSearch(nums, middle + 1, right, target)
-        else:
-            return self.binSearch(nums, left, middle - 1, target)
-
     def search(self, nums: List[int], target: int) -> int:
-        return self.binSearch(nums, 0, len(nums) - 1, target)
-        
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            # middle = left + (right - left) // 2
+            # For capped int languages like Java
+            middle = (left + right) // 2
+
+
+            if nums[middle] == target:
+                return middle
+            
+            if nums[middle] < target:
+                left = middle + 1
+
+            elif nums[middle] > target:
+                right = middle - 1
+        return -1
