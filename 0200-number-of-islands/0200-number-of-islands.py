621da293251dda7205850f24c36bdecccdffc276
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-11-17 22:03:57 -0800

    Time: 280 ms (20.92%), Space: 28 MB (14.4%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,7 +4,7 @@ class Solution:
         count = 0
         directions = [(0, -1), (-1, 0), (1, 0), (0, 1)]
 
-        def bfs(r, c):
+        def dfs(r, c):
             if (r, c) in visited:
                 return
             
@@ -13,14 +13,14 @@ class Solution:
             for dr, dc in directions:
                 nr, nc = dr + r, dc + c
                 if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == "1":
-                    bfs(nr, nc)
+                    dfs(nr, nc)
 
 
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if (r, c) not in visited and grid[r][c] == "1":
-                    bfs(r, c)
+                    dfs(r, c)
                     count += 1
         
         return count
