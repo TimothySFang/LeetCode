a202aa9f06c6ac361583e74770a2ef3c825c9195
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-11-19 16:01:26 -0800

    Time: 298 ms (13.06%), Space: 25.3 MB (26.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         visited = set()
+        def bfs(curr_node):
+            queue = deque()
+            queue.append(curr_node)            
+            while queue:
+                curr_node = queue.popleft()
+                if curr_node in visited:
+                    continue
+
+                visited.add(curr_node)
+                if grid[curr_node[0]][curr_node[1]] == "0":
+                    continue
+                
+                for dr, dc in directions:
+                    nr, nc = curr_node[0] + dr, curr_node[1] + dc
+                    if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                        if grid[nr][nc] != "0":
+                            queue.append((nr, nc))
+
         count = 0
-        directions = [(0, -1), (-1, 0), (1, 0), (0, 1)]
-
-        def dfs(r, c):
-            if (r, c) in visited:
-                return
-            
-            visited.add((r, c))
-
-            for dr, dc in directions:
-                nr, nc = dr + r, dc + c
-                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == "1":
-                    dfs(nr, nc)
-
-
-
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if (r, c) not in visited and grid[r][c] == "1":
-                    dfs(r, c)
                     count += 1
-        
+                    bfs((r, c))
         return count
