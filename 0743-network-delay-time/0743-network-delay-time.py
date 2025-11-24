d7d68e29d657d4602f41486c9bfed729a64efa51
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-11-23 18:19:31 -0800

    Time: 373 ms (76.54%), Space: 19.7 MB (75.92%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,28 +1,26 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = [[] for _ in range(n + 1)]
-        for u, v, w in times:
-            graph[u].append((v, w))
-        dist = [inf] * (n + 1)
-        dist[k] = 0
+        
+        cost = [float('inf')] * (n + 1)
+        cost[k] = 0
+        graph = defaultdict(list)
+
+        for t in times:
+            graph[t[0]].append((t[1], t[2])) #source node = (target node, weight)
 
         heap = [(0, k)]
 
         while heap:
-            curr_time, u = heapq.heappop(heap)
+            curr_time, node = heapq.heappop(heap)
 
-            if curr_time > dist[u]:
+            if curr_time > cost[node]:
                 continue
-
-            if curr_time > dist[u]:
-                continue
-
-            for v, w in graph[u]:
-                new_time = curr_time + w
-                if new_time < dist[v]:
-                    dist[v] = new_time
-                    heapq.heappush(heap, (new_time, v))
-                
-        max_dist = max(dist[1:])  # ignore index 0
+    
+            for u, w in graph[node]:
+                # u = node, w = weight
+                if curr_time + w < cost[u]:
+                    cost[u] = curr_time + w
+                    heapq.heappush(heap, (curr_time + w, u))
         
-        return -1 if max_dist == inf else max_dist
+
+        return -1 if max(cost[1:]) == float('inf') else max(cost[1:])
