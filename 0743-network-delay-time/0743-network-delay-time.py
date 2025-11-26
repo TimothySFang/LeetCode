41f3c09d781c51610e67709369b4bb6495e94f94
Timothy Fang <56328137+TimothySFang@users.noreply.github.com>
2025-11-25 17:08:18 -0800

    Time: 361 ms (95.74%), Space: 19.5 MB (92.43%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,33 +1,27 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = defaultdict(list)
-        for u, v, w in times:
-            graph[u].append((v, w))
-        
-        cost = [float('inf')] * (n + 1) # cost array at n + 1 length because nodes go from 1 to n
+        cost = [float('inf')] * (n + 1)
         cost[k] = 0
-        heap = [(0, k)] # heap for graph traversal where each value is (cost, node) 
+        heap = [(0, k)]
+        for u, v, w in times:
+            graph[u].append((w, v))
 
+        
         while heap:
             weight, curr_node = heapq.heappop(heap)
-
+            
             if weight > cost[curr_node]:
-                # reduce looping if this one is already the "heavier path"
-                continue
+                continue 
 
-            for v, w in graph[curr_node]:
-                # looping through all the neighbor nodes of curr_node
+            for child_weight, child_node in graph[curr_node]:
+                if child_weight + weight < cost[child_node]:
+                    cost[child_node] = child_weight + weight
+                    heapq.heappush(heap, (child_weight + weight, child_node))
 
-                if w + weight < cost[v]:
-                    # this path is shorter
-                    cost[v] = w + weight
-                    heapq.heappush(heap, (w + weight, v))
         print(cost)
-        for i in range(1, len(cost)):
-            if cost[i] >= float('inf'):
+        for c in cost[1:]:
+            if c >= float('inf'):
                 return -1
-    
         return max(cost[1:])
-
-
 
